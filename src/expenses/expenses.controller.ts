6479a792958c03615197489cfe73201ac40aed80
diogorcarvalho <diogorcarvalho@gmail.com>
2025-02-22 19:07:55 -0300

    feat(End-point para deletar):

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 import { IExpenseEntity } from 'src/domain/entities/IExpense';
 import { ExpensesRepository } from 'src/repositories/expenses.repository';
@@ -19,18 +19,24 @@ export class RegistryExpenseRequest {
 export class ExpensesController {
     constructor(
         private readonly createExpenseUseCase: CreateExpenseUseCase,
-        private readonly createExpensesRepository: ExpensesRepository,
+        private readonly expensesRepository: ExpensesRepository,
     ) { }
 
     @Get()
     @HttpCode(HttpStatus.OK)
     async findAll(): Promise<IExpenseEntity[]> {
-        return this.createExpensesRepository.findAll();
+        return this.expensesRepository.findAll();
     }
 
     @Post('registrar')
     @HttpCode(HttpStatus.OK)
     async create(@Body() expense: RegistryExpenseRequest): Promise<void> {
         return await this.createExpenseUseCase.execute(expense);
+    }
+
+    @Delete('eliminar/:id')
+    @HttpCode(HttpStatus.OK)
+    async delete(@Param('id') id: string): Promise<void>  {
+        return this.expensesRepository.delete(id);
     }
 }
